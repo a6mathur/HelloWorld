21897bc7eaa4e8e6bc74f9eb80068a13f79c402e
anshul <mathur.anshul@gmail.com>
2019-05-05 00:38:18 +0530

    add array copy statement

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -3,7 +3,10 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		int[] x = {10,20,30};
+		int[] y = new int[3];		
+		System.arraycopy(x, 0, y, 0, x.length);
+		System.out.println(y[2]);
 	}
 
 }
